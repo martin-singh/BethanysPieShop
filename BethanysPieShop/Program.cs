98a2a13cd22cd50d8a7e54087a491fd1e2adc6f8
martin-singh <martin.singhvirk@outlook.com>
2021-08-16 11:43:15 +0200

    Section 3 - Setting up an MVC Application.

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -13,14 +13,16 @@ namespace BethanysPieShop
     {
         public static void Main(string[] args)
         {
+            // Host is behing set up, which will configure a server and a request processing pipeline.
             CreateHostBuilder(args).Build().Run();
         }
 
+        // Will setup the app with some defaults, which is hosted by its internal server enabled by ISS.
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>(); // Startup -> configuration of the app.
                 });
     }
 }
--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -10,30 +10,38 @@ using System.Threading.Tasks;
 
 namespace BethanysPieShop
 {
+    // Startup -> 2 things mainly: Define the request handling pipeline and configure all services needed thru out the app.
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Register services here (through dependency injection):
+            services.AddControllersWithViews(); // Bring in support for working with MVC.
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Add middleware components here (order matters):
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection(); // Redirects HTTP-requests to HTTPS.
+
+            app.UseStaticFiles(); // Serves static files such as JS, CSS, images etc.
+            
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                // Responsible to map a incoming request to a action of a controller.
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
